f97d3818d959b131b2b2e77f566536d24cabb3a9
DanielDaffner <danieldaffner@outlook.de>
2020-11-21 19:41:13 +0100

    Session 1: Multiplayer Room, Player Spawn, Player Movement
    Max 4 Players
    
    Fitfth player gets new lobby but no spawn
    
    Photon imported
    Cinemachine imported

---
+++ StarHunter/Assets/Scripts/MyPhoton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MyPhoton : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        GameObject newPlayer = PhotonNetwork.Instantiate("Character1", Vector3.zero, Quaternion.identity);
+        // for Camera -- GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>().Follow = newPlayer.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
