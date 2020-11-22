0fd66ab6a478c31afa554143aefc35042ec41ecd
DanielDaffner <danieldaffner@outlook.de>
2020-11-22 01:41:34 +0100

    Gravity & Jump

--- StarHunter/Assets/Scripts/CharacterMovement.cs
+++ StarHunter/Assets/Scripts/CharacterMovement.cs
@@ -10,9 +10,19 @@ public class CharacterMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
 
     PhotonView photonView;
 
@@ -25,8 +35,10 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         if (!photonView.IsMine) return;
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
@@ -36,5 +48,25 @@ public class CharacterMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             controller.Move(direction * speed * Time.deltaTime);
         }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (!isGrounded)
+        {
+            print("Mid Air!");
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        
     }
 }
