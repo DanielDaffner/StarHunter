646c06731241bbc0481328d5cdf271c8d56d3ec6
DanielDaffner <danieldaffner@outlook.de>
2020-12-06 20:12:04 +0100

    Update PlayerCollision_Own.cs

--- StarHunter/Assets/Scripts/PlayerCollision_Own.cs
+++ StarHunter/Assets/Scripts/PlayerCollision_Own.cs
@@ -18,6 +18,7 @@ public class PlayerCollision_Own : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if(!other.transform.Find("StarGhost").GetComponent<MeshRenderer>().enabled) return ;
+        if (other.isTrigger) return;
         otherTmp = other;
     }
 
